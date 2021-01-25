baea9c4dd622b477551049b61db14bdc9260f6c9
Anastasia Dashkovska <anastasiia1529@gmail.com>
2021-01-25 14:21:34 +0200

    Create templates

---
+++ templates/footer.php
@@ -0,0 +1,16 @@
+<footer class="container-fluid">
+    <div class="container align">
+        <p><a class="navbar-brand logo" href="#">HOME LIBRARY</a></p>
+        <div class="footer-menu">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Main Page <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">All Books</a>
+                </li>
+            </ul>
+        </div>
+        <div class="created-by"> @created by me</div>
+    </div>
+</footer>
