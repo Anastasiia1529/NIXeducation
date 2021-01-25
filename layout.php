2e519c884cbe15119846bd48bc836c3cdcb98a49
Anastasia Dashkovska <anastasiia1529@gmail.com>
2021-01-25 14:57:17 +0200

    Add layout

---
+++ layout.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <title>Home Library</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="./css/style.css">
+  </head>
+  <body>
+    <?php
+      include_once __DIR__ . '/./templates/header.php';
+    ?>
+    <?php
+      echo 'Content'
+    ?>
+    <?php
+    include_once __DIR__ . '/./templates/footer.php';
+    ?>
+  </body>
+</html>
