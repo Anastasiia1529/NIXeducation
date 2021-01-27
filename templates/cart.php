4109796e29d3f51e33336a89bf8d937dfaa1e9f0
Anastasia Dashkovska <anastasiia1529@gmail.com>
2021-01-27 14:04:43 +0200

    Update teplates

---
+++ templates/cart.php
@@ -0,0 +1,22 @@
+<div class="container wrapper">
+    <h1>Your shopping cart</h1>
+    <ul class="cart-list">
+        <li class="product-list">
+            <img src="./Img/harry-potter.jpeg">
+            <div>
+                <p>Harry Potter and the Philosopher's Stone</p>
+                <p>J. K. Rowlling</p>
+            </div>
+            <div class="product-price">10,35 $</div>
+        </li>
+        <li class="product-list">
+            <img src="./Img/harry-potter.jpeg">
+            <div>
+                <p>Harry Potter and the Philosopher's Stone</p>
+                <p>J. K. Rowlling</p>
+            </div>
+            <div class="product-price">10,35 $</div>
+        </li>
+    </ul>
+    <button class="btn btn-checkout">Checkout</button>
+</div>
