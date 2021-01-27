7ce0bf3d4ebd118627fb644acbaafd9ab89f7017
Anastasia Dashkovska <anastasiia1529@gmail.com>
2021-01-27 13:28:42 +0200

    Create storage with products

---
+++ Data/products.php
@@ -0,0 +1,66 @@
+<?php
+return [
+    [
+        "id" => 1,
+        "title" => "Queen's Gambit",
+        "author" => "Walter Tevis ",
+        "image" => "/../../Img/gambit.jpeg",
+        "price" => "7,77 $"
+    ],
+    [
+        "id" => 2,
+        "title" => "The Alchemist",
+        "author" => "Paulo Coelho ",
+        "image" => "/../../Img/alchemist.jpeg",
+        "price" => "7,35 $"
+    ],
+    [
+        "id" => 3,
+        "title" => "Harry Potter and the Philosopher's Stone",
+        "author" => "J. K. Rowlling ",
+        "image" => "/../../Img/harry-potter.jpeg",
+        "price" => "10,35 $"
+    ],
+    [
+        "id" => 4,
+        "title" => "Alice in Wonderland",
+        "author" => "Lewis Carroll ",
+        "image" => "/../../Img/alice.jpeg",
+        "price" => "8,50 $"
+    ],
+    [
+        "id" => 5,
+        "title" => "The Little Prince",
+        "author" => "Antoine de Saint-Exupery ",
+        "image" => "/../../Img/little-prince.jpeg",
+        "price" => "3,70 $"
+    ],
+    [
+        "id" => 6,
+        "title" => "Lovely Bones",
+        "author" => "Alice Sebold",
+        "image" => "/../../Img/lovely-bones.jpeg",
+        "price" => "8,40 $"
+    ],
+    [
+        "id" => 7,
+        "title" => "The Colour Of Magic",
+        "author" => "Terry Pratchett ",
+        "image" => "/../../Img/colour-of-magic.jpeg",
+        "price" => "10,00 $"
+    ],
+    [
+        "id" => 8,
+        "title" => "The Humans",
+        "author" => "Matt Haig",
+        "image" => "/../../Img/the-humans.png",
+        "price" => "8,50 $"
+    ],
+    [
+        "id" => 9,
+        "title" => "Murder on the Orient Express",
+        "author" => "Agatha Christie ",
+        "image" => "/../../Img/murder-in-express.jpeg",
+        "price" => "8,40 $"
+    ]
+];
